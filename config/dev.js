1177ab531a47500043b7779c69c585eaea061297
Aditya Bora <addybora07@gmail.com>
2021-05-20 23:47:29 +0530

    private info to be secured

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports={
+  mongoURI:'mongodb+srv://AdityaBora:NtGI9iVzpEETRJgK@reactapp.xmrk9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://AdityaBora:NtGI9iVzpEETRJgK@reactapp.xmrk9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true}).then(function () {
+const bodyParser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const { User }=require('./models/user');
+const config=require('./config/key');
+
+mongoose.connect(config.mongoURI,{useNewUrlParser:true,useUnifiedTopology:true}).then(function () {
   console.log('success');
 });
-app.get('/',function(req,res){
-  res.send('hello world');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register',function (req,res) {
+    const user=new User(req.body);
+    user.save(function(err,userData){
+      if(err)return res.json({success:false,err});
+        return res.status(200).json({success:true});
+    });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(5000);
